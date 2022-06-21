9d2b6104a6cef73b8e84d006ac4102f2a9f66534
Sol-ha <101246194+Sol-Ha@users.noreply.github.com>
2022-06-21 12:15:11 +0200

    Update class_product.js

--- scripts/class_product.js
+++ scripts/class_product.js
@@ -1,34 +1,16 @@
 class Product extends Category{
-  constructor(name, nom, price, promotion, discount) {
+  constructor(name, category, price, promotion, discount) {
     super(name);
-    this.nom = nom;
+    this.category = category;
     this.price = price;
     this.promotion = promotion;
     this.discount = discount;
   }
-// setName(n){
-//   this.name = n;
-// }
-//   template literals
-  // feedback() {
-  //   console.log(`Give me ${this.name} for ${this.promotion}`);
-  // }
-  // command to add to price
-  // addPrice(changeTotal) {
-  //   this.price += changeTotal;
-  // }
-  // command to lower to price
-  // lowerPrice(changeTotal) {
-  //   this.price -= changeTotal;
-  // }
 
   // calling the method directly inside the list
-  
+
   toList(){
-    let result = [this.nom, super.name, this.price +` €`, this.promotion, this.discount];
+    let result = [this.name, this.category, this.price +` €`, this.promotion, this.discount];
     return result;
   }
-
-
 }
-
